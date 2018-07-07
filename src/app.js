b41e838c5f8943ff9be1ba29b9cfbbda51660840
Rajendra Singh <rajendrasinghrajput@gmail.com>
2018-07-07 10:15:39 -0700

    Added -
    1. Linting
    2. EditorConfig
    3. EJS - View Engine
    4. npmrc - NPM configuration with exact version save
    5. nodemon json file

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Node Js!');
+  res.render('index', { title: 'My Library', list: ['a', 'b'] });
 });
 
 app.listen(PORT, () => {
-    if(error) {
-        console.log(chalk.red(error));
-    } else {
-        console.log(chalk.green(`** Development Server is listening on localhost:${PORT}, open your browser on http://localhost:${PORT}/ **`));
-    }
+  debug(chalk.green(`** Development Server is listening on localhost:${PORT}, open your browser on http://localhost:${PORT}/ **`));
 });
